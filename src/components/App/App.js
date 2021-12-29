a43557fd6d6c8b551bacc4b5a6c398e04e01a83a
Katie <pie10101011@gmail.com>
2021-12-29 14:55:34 -0800

    Updated App and started working on SearchBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
+
+import { SearchBar } from '../SearchBar/SearchBar'
 
 function App() {
   return (
@@ -17,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
+      <SearchBar />
       </header>
+
     </div>
   );
 }
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component {
+    renderSortByOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>;
+            
+        });
+    }
+    render() {
+        
+    }
+}
+
+export default SearchBar;
