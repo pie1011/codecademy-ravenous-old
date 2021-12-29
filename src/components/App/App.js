bcc92d36315a98730bfb201e39d25c034d1cb88f
Katie <pie10101011@gmail.com>
2021-12-29 15:23:34 -0800

    Part one completed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,19 @@
-import logo from '../../logo.svg';
 import './App.css';
 
-import { SearchBar } from '../SearchBar/SearchBar'
+import React from 'react';
+import BusinessList from '../BusinessList/BusinessList'
+import SearchBar from '../SearchBar/SearchBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <SearchBar />
-      </header>
-
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -12,11 +12,27 @@ class SearchBar extends React.Component {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
             return <li key={sortByOptionValue}>{sortByOption}</li>;
-            
+
         });
     }
     render() {
-        
+        return(
+            <div className="SearchBar">
+            <div className="SearchBar-sort-options">
+                <ul>
+                    {this.renderSortByOptions}
+                </ul>
+            </div>
+            <div className="SearchBar-fields">
+                <input placeholder="Search Businesses" />
+                <input placeholder="Where?" />
+            </div>
+            <div className="SearchBar-submit">
+                <a>Let's Go</a>
+            </div>
+        </div>
+        )
+
     }
 }
 
